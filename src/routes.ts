c825250960d03c1505efaffa3abe47a3d81798ae
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2021-02-25 11:03:28 -0300

    Template creation and sending email

--- src/routes.ts
+++ src/routes.ts
@@ -2,16 +2,20 @@ import { Router } from 'express';
 
 import { UserController } from './controllers/UserController';
 import { SurveyController } from './controllers/SurveyController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const usersController = new UserController();
 const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 
 router.post('/users', usersController.create);
 
 router.get('/surveys', surveyController.show);
 router.post('/surveys', surveyController.create);
+
+router.post('/sendmail', sendMailController.execute);
 
 export { router };
 
